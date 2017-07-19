fc702d10d23384b0ee535b0077a376cd45f872d0
Eric Masiello <eric.j.masiello@gmail.com>
2017-07-19 19:16:38 -0400

    Bug fixes for preferred styles. Include support for sass directives

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: 'stylelint-config-standard', 
   rules: {
     'selector-max-id': 0,
-    'selector-max-compound-selectors': 3,
+    'selector-max-compound-selectors': 0,
     'selector-max-specificity': '0,3,3',
-  }
+    'at-rule-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'at-rule-no-unknown': [true, {
+      ignoreAtRules: [
+        'mixin',
+        'extend',
+        'include',
+        'function',
+        'return',
+        'if',
+        'else',
+        'for',
+        'while',
+        'each',
+        'content',
+        'at-root',
+        'debug',
+        'warn',
+        'error',
+      ],
+    }],
+  },
 };
