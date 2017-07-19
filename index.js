79007fbaaaf8ed3a43040b950551b0ceeba7be7e
Eric Masiello <eric.j.masiello@gmail.com>
2017-07-19 19:17:39 -0400

    Fix selector-max-compound-selectors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'stylelint-config-standard', 
   rules: {
     'selector-max-id': 0,
-    'selector-max-compound-selectors': 0,
+    'selector-max-compound-selectors': 3,
     'selector-max-specificity': '0,3,3',
     'at-rule-empty-line-before': null,
     'declaration-empty-line-before': null,
